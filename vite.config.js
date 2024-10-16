45bd69737803e78c60e43b28223c0aa6d977eda3
Madhan235 <117820919+Madhan235@users.noreply.github.com>
2024-10-16 16:55:16 +0530

    server url changed

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://settyl-server.vercel.app/",
+        target: "https://settyl-server.vercel.app",
         changeOrigin: true,
         secure: false,
       },
